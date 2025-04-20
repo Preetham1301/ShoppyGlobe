48eba90a3cc249096b56cd0b836d69dd596fe2b0
Preetham1301 <pretham2345@gmail.com>
2025-04-20 14:57:30 +0530

    Refactor code structure for improved readability and maintainability

--- src/main.jsx
+++ src/main.jsx
@@ -1,7 +1,6 @@
 /* eslint-disable no-unused-vars */
 import { lazy, StrictMode, Suspense } from 'react'
 import { createRoot } from 'react-dom/client'
-import './index.css'
 import { createBrowserRouter, RouterProvider } from 'react-router-dom'
 import { Provider } from 'react-redux'
 import AppStore from './utilities/appStore'
