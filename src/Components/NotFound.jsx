4874a717d96c77754c6152b57508eb47b5b0bc37
Preetham1301 <pretham2345@gmail.com>
2025-04-20 14:46:40 +0530

    Add main components: App, Body, Cart, CartItem, Checkout, Header, NotFound, ProductDetails, ProductItem, and ProductList with initial functionality

---
+++ src/Components/NotFound.jsx
@@ -0,0 +1,19 @@
+// Components/NotFound.js
+import React from 'react';
+import { useNavigate } from 'react-router-dom';
+
+ const NotFound = () => {
+  const navigate = useNavigate()
+  return (
+    <>
+    <div className="border-4 flex-col align-middle items-center justify-center h-screen w-screen">
+      <h1 className="text-4xl">404 - Page Not Found</h1>
+      <p>The page you are looking for does not exist.</p>
+     <button onClick={() => navigate("/")} className='border-2 px-5 bg-orange-300 rounded-2xl py-2 hover:text-black text-white'>Go Back</button>  
+    </div>
+    </>
+  
+  );
+};
+
+export default NotFound
